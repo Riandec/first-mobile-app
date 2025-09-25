ebb5cbaa46ad268ad45e2073e9b9f16d6e1808d4
Riandec <studpitch65@gmail.com>
2025-09-25 11:42:35 +0700

    Update Assignment

--- lib/api/api_assignment.dart
+++ lib/api/api_assignment.dart
@@ -21,7 +21,11 @@ class _ApiAssignState extends State<ApiAssign> {
 
   void fetchData() async {
     try {
-      var response = await http.get(Uri.parse('https://api.waqi.info/feed/here/?token=6098441d02eb4e942e7af57172aa969162c67982'));
+      var response = await http.get(
+        Uri.parse(
+          'https://api.waqi.info/feed/here/?token=6098441d02eb4e942e7af57172aa969162c67982',
+        ),
+      );
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body);
         Aqi aqi = Aqi.fromJson(data);
@@ -33,7 +37,9 @@ class _ApiAssignState extends State<ApiAssign> {
           aqiData = null;
         });
         print('Failed to fetch data');
-        print('Status code: ${response.statusCode} Response body: ${response.body}');
+        print(
+          'Status code: ${response.statusCode} Response body: ${response.body}',
+        );
       }
     } catch (e) {
       print('Error: $e');
@@ -43,24 +49,29 @@ class _ApiAssignState extends State<ApiAssign> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Today's AQI & Temp")
-      ),
-      body: Container(
-        padding: EdgeInsets.only(left: 15),
-        decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Icon(Icons.location_pin),
-            SizedBox(width: 5),
-            Text('${aqiData?.city}', style: TextStyle(fontSize: 20)),
-            Column(
-              
+      appBar: AppBar(title: const Text("Today's AQI & Temp")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(height: 30, decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.location_pin),
+                  SizedBox(width: 5),
+                  Text('${aqiData?.city}', style: TextStyle(fontSize: 20))
+                ],
+              )
             )
-          ]
-        )
-      )
+          ),
+          SizedBox(height: 30),
+          Container(height: 200, width: 400, decoration: BoxDecoration(border: Border.all(color: Colors.green))),
+          SizedBox(height: 30),
+          Container(height: 200, width: 400, decoration: BoxDecoration(border: Border.all(color: Colors.blue))),
+        ],
+      ),
     );
   }
 }
