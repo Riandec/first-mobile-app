038b2d3f89a84a2b66545acbfd6bfc33a067f52b
Riandec <studpitch65@gmail.com>
2025-09-26 01:02:04 +0700

    Update Assignment

--- lib/api/api_assignment.dart
+++ lib/api/api_assignment.dart
@@ -66,7 +66,8 @@ class _ApiAssignState extends State<ApiAssign> {
             )
           ),
           SizedBox(height: 20),
-          // Temp
+          
+          // temp
           Stack(
             children: [
               Container(
@@ -81,10 +82,7 @@ class _ApiAssignState extends State<ApiAssign> {
                     Expanded(
                       child: Container(
                         alignment: Alignment(-1.0, -1.0),
-                        padding: EdgeInsets.only(top: 10, left: 20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.red), 
-                        ),
+                        padding: EdgeInsets.only(top: 5, left: 20),
                         child: Text('Temperature', style: TextStyle(fontSize: 20))
                       )
                     ),
@@ -92,20 +90,14 @@ class _ApiAssignState extends State<ApiAssign> {
                       flex: 2,
                       child: Container(
                         alignment: Alignment(-1.0, -1.0),
-                        padding: EdgeInsets.only(top: 10, left: 20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.orange), 
-                        ),
+                        padding: EdgeInsets.only(top: 5, left: 20),
                         child: Text('${aqiData?.temp} °c', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))
                       )
                     ),
                     Expanded(
                       child: Container(
-                        alignment: Alignment(-1.0, -1.0),
+                        alignment: Alignment(-1.0, -2.0),
                         padding: EdgeInsets.only(left: 20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.yellow), 
-                        ),
                         child: Text('Partly Cloudy', style: TextStyle(fontSize: 20))
                       )
                     ),
@@ -113,17 +105,19 @@ class _ApiAssignState extends State<ApiAssign> {
                 )
               ),
               Positioned(
+                top: 10,
                 right: 10,
                 child: Image.asset('../assets/images/cloudy.png', height: 120)
               )
             ],
           ),
           SizedBox(height: 20),
-          // AQI
+          
+          // aqi
           Stack(
             children: [
               Container(
-                height: 150, 
+                height: 200, 
                 width: 350, 
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.black), 
@@ -134,10 +128,7 @@ class _ApiAssignState extends State<ApiAssign> {
                     Expanded(
                       child: Container(
                         alignment: Alignment(-1.0, -1.0),
-                        padding: EdgeInsets.only(top: 10, left: 20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.red), 
-                        ),
+                        padding: EdgeInsets.only(top: 5, left: 20),
                         child: Text('Air Quality Index (AQI)', style: TextStyle(fontSize: 20))
                       )
                     ),
@@ -145,48 +136,197 @@ class _ApiAssignState extends State<ApiAssign> {
                       flex: 2,
                       child: Container(
                         alignment: Alignment(-1.0,-1.0),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.orange), 
-                        ),
-                        child: ListTile(
-                          contentPadding: EdgeInsets.only(left: 20),
-                          title: Text('${aqiData?.aqi}', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
-                          subtitle: Text('Good', style: TextStyle(fontSize: 15))
+                        padding: EdgeInsets.only(left: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('${aqiData?.aqi}', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                            Text('Good', style: TextStyle(fontSize: 20)),
+                          ],
                         )
                       )
                     ),
                     Expanded(
                       child: Container(
-                        alignment: Alignment(-1.0, -1.0),
-                        padding: EdgeInsets.only(top: 10, left: 20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.yellow), 
-                        ),
+                        margin: EdgeInsetsGeometry.only(left: 20, right: 20, top: 5, bottom: 15),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black),
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(30),
+                                    bottomLeft: Radius.circular(30)
+                                  ),
+                                  color: Color.fromARGB(255,162,239,96)
+                                ),
+                                child: Text('50', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                              )
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black), 
+                                  color: Color.fromARGB(255,254,235,80)
+                                ),
+                                child: Text('100', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                              )
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black), 
+                                  color: Color.fromARGB(255,253,178,76)
+                                ),
+                                child: Text('150', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                              )
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black), 
+                                  color: Color.fromARGB(255,255,97,117)
+                                ),
+                                child: Text('200', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                              )
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black), 
+                                  color: Color.fromARGB(255,179,111,189)
+                                ),
+                                child: Text('300', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                              )
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black), 
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(30),
+                                    bottomRight: Radius.circular(30)
+                                  ),
+                                  color: Color.fromARGB(255,147,83,116)
+                                ),
+                                child: Text('500', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
+                              )
+                            )
+                          ],
+                        )
                       )
                     ),
                   ],
                 )
               ),
               Positioned(
-                top: 40,
-                right: 35,
-                child: Image.asset('../assets/images/good.png', height: 80)
+                top: 45,
+                right: 30,
+                child: Image.asset('../assets/images/good.png', height: 90)
               )
             ],
           ),
           SizedBox(height: 20),
+          
           // weather info
           Container(
             height: 150, 
-            width: 400, 
+            width: 350, 
             decoration: BoxDecoration(
               border: Border.all(color: Colors.black), 
               borderRadius: BorderRadius.circular(15)
             ),
-            // child:
-          ),
-        ],
-      ),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment(-1.0, -1.0),
+                    padding: EdgeInsets.only(top: 5, left: 20),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red), 
+                    ),
+                    child: Text('Weather Information', style: TextStyle(fontSize: 20))
+                  )
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    alignment: Alignment(-1.0, -1.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red), 
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.green), 
+
+                                  ),
+                                )
+                              ),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.green), 
+
+                                  ),
+                                )
+                              ),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.green), 
+
+                                  ),
+                                )
+                              )
+                            ],
+                          )
+                        ),
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.green), 
+
+                                  ),
+                                )
+                              ),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.green), 
+
+                                  ),
+                                )
+                              ),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.green), 
+
+                                  ),
+                                )
+                              )
+                            ],
+                          )
+                        ),
+                      ],
+                    )
+                  )
+                ),
+              ]
+            )
+          )
+        ]
+      )
     );
   }
 }
